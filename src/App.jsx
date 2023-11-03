c61c4e6a521cdba942d4cf00f9d12a2b431401bb
ooiws <ooiwensong@gmail.com>
2023-11-03 16:07:50 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import './App.jsx';
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-6xl text-sky-400">Hello World</h1>
+    </>
+  );
+}
+
+export default App;
